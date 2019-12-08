d97f7cfb3d4502ca39b4f0e356e8724b9acfff85
Ville Ranki <ville.ranki@iki.fi>
2019-12-08 22:49:34 +0200

    Add -u to get docker logs working and --noreload to avoid CPU hogging

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,5 @@ echo "Creating superuser"
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${ADMIN_USER:-admin}', '${ADMIN_EMAIL:-admin@email.invalid}', '${ADMIN_PASSWORD:-password}')" | python manage.py shell
 
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python -u manage.py runserver 0.0.0.0:8000 --noreload
+
